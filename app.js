92f16985ef1878224877ed19d93e43bb9f8cb1ce
Divya Namdev <divyanamdev8795@gmail.com>
2021-08-16 10:30:51 +0530

    js imported

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("script is working from different file");
+
+// input
+var username = prompt("tell me your name")
+// processing
+var welcomeMessage = "Hii " + username;
+
+// output
+alert(welcomeMessage);
